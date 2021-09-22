5a81dd1e8346570bef234ba9a01af5857d6bdbfc
WdvOps <dvops@outlook.com>
2021-09-22 11:31:31 -0300

    Feat: Ajuste de configuração da API '@CrossOrigin'

--- os/src/main/java/com/will/os/resources/TecnicoResource.java
+++ os/src/main/java/com/will/os/resources/TecnicoResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import com.will.os.domain.Tecnico;
 import com.will.os.dtos.TecnicoDTO;
 import com.will.os.services.TecnicoService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/tecnicos")
 public class TecnicoResource {
